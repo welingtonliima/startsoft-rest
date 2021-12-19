d3071629718c44b5b75e4e779c091c74e5c1c600
welingtonliima <welington.lima93@gmail.com>
2021-12-19 19:44:28 -0300

    ci:configurando intregração contínua

--- src/main/java/wjangoo/io/startsoft/controllers/SampleController.java
+++ src/main/java/wjangoo/io/startsoft/controllers/SampleController.java
@@ -1,12 +1,10 @@
 package wjangoo.io.startsoft.controllers;
 
-import org.apache.catalina.connector.Response;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
-import org.springframework.web.bind.annotation.RequestParam;
 
 
 @RestController
