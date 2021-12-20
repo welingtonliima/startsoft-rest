589610f5b35da0a6176e726cd2f1da5d7ca5489e
welingtonliima <welington.lima93@gmail.com>
2021-12-20 01:05:19 -0300

    ci: configurando integração contínua

--- src/main/java/wjangoo/io/startsoft/controllers/SampleController.java
+++ src/main/java/wjangoo/io/startsoft/controllers/SampleController.java
@@ -1,7 +1,5 @@
 package wjangoo.io.startsoft.controllers;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.core.env.Environment;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
